747c0406ea049c87971ab1fabf4cada7094ba003
bolin <1607944055@qq.com>
2024-11-30 15:50:56 +0800

    测试

---
+++ src/test/java/com/bolin/service/impl/ScoringResultServiceImplTest.java
@@ -0,0 +1,21 @@
+package com.bolin.service.impl;
+
+import com.bolin.service.ScoringResultService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ScoringResultServiceImplTest {
+
+    @Autowired
+    private ScoringResultService scoringResultService;
+
+    @Test
+    public  void test1(){
+        scoringResultService.test1();
+    }
+
+}
