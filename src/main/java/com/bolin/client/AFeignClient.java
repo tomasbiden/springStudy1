d9261308466df235faf991fa5fb5d7674fb83f83
bolin <1607944055@qq.com>
2025-01-12 15:38:09 +0800

    加入负载均衡loadbalancer依赖，与通过nacos调用服务

--- src/main/java/com/bolin/client/AFeignClient.java
+++ src/main/java/com/bolin/client/AFeignClient.java
@@ -3,11 +3,14 @@ package com.bolin.client;
 import com.bolin.common.BaseResponse;
 import com.bolin.model.dto.app.AppQueryRequest;
 import com.bolin.model.vo.AppVO;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
 
-@FeignClient(url = "http://localhost:8081/api/app",name = "feignCustomerService")
+//@FeignClient(url = "http://localhost:8081/api/app",name = "yudada-backend")
+@FeignClient(name = "aiScore")
+//@EnableFeignClients
 public interface AFeignClient {
     @GetMapping("/test")
 //    @Headers(value = "Content-Type: application/json")
