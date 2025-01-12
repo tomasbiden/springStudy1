7fce0b228991ae300371095d97935d30b005fa01
bolin <1607944055@qq.com>
2025-01-12 15:01:20 +0800

    引入spiring cloud注释，讲服务注册到nacos上

--- src/main/java/com/bolin/Application.java
+++ src/main/java/com/bolin/Application.java
@@ -3,6 +3,7 @@ package com.bolin;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @MapperScan("com.bolin.mapper")
 @EnableAspectJAutoProxy(proxyTargetClass = false) // 使用 JDK 动态代理
 @EnableFeignClients(basePackages = "com.bolin.client")
+@EnableDiscoveryClient
 public class Application {
 
     public static void main(String[] args) {
