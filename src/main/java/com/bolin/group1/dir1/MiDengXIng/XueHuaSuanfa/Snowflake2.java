68bd678fa30c6c7e607b9d5a60a6a0508d655933
bolin <1607944055@qq.com>
2025-02-16 22:40:15 +0800

    雪花算法更换名字

--- src/main/java/com/bolin/group1/dir1/MiDengXIng/XueHuaSuanfa/Snowflake2.java
+++ src/main/java/com/bolin/group1/dir1/MiDengXIng/XueHuaSuanfa/Snowflake2.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 
 @Component
-public class Snowflake {
+public class Snowflake2 {
 
     public void try1(){
 
--- src/test/java/com/bolin/group1/MiDengXIng/XueHuaSuanfa/XueHuaSuanfa1Test.java
+++ src/test/java/com/bolin/group1/MiDengXIng/XueHuaSuanfa/XueHuaSuanfa1Test.java
@@ -1,6 +1,6 @@
 package com.bolin.group1.MiDengXIng.XueHuaSuanfa;
 
-import com.bolin.group1.dir1.MiDengXIng.XueHuaSuanfa.Snowflake;
+import com.bolin.group1.dir1.MiDengXIng.XueHuaSuanfa.Snowflake2;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,7 +13,7 @@ class XueHuaSuanfa1Test {
 
 //    添加个springbootTest就没有问题了啊
     @Autowired
-Snowflake xueHuaSuanfa1;
+Snowflake2 xueHuaSuanfa1;
 
 
     @BeforeEach
