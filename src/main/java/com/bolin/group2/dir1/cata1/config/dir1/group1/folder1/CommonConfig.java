1d47a361a8582942482496b1efb9ae06ea7f7641
bolin <1607944055@qq.com>
2025-03-01 21:16:05 +0800

    降低myabtis-plus 版本,加入mybaits-plus page 拦截器

---
+++ src/main/java/com/bolin/group2/dir1/cata1/config/dir1/group1/folder1/CommonConfig.java
@@ -0,0 +1,20 @@
+package com.bolin.group2.dir1.cata1.config.dir1.group1.folder1;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CommonConfig {
+    /**
+     * MyBatisPlus拦截器（用于分页）
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return interceptor;
+    }
+}
