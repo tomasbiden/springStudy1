84236ee0400a31e392167ef96d922b2f23ff5db2
bolin <1607944055@qq.com>
2025-03-02 09:18:51 +0800

    deepPageSql

--- sql/group1/dir1/cata1/folder1/deepPage.sql
+++ sql/group1/dir1/cata1/folder1/deepPage.sql
@@ -9,3 +9,53 @@ select *
 
 from  user_answer_0
 order by id limit  5000000,10
+
+select *
+from user_answer_0
+where id =1893230945074315264
+
+
+select count(*)
+from user_answer_0
+
+
+explain
+select *
+from user_answer_0  limit 1,20
+
+select *
+from user_answer_0  father_table
+where exists( where father_table.id=tmp_table .id)
+
+# 错误
+select *
+from user_answer_0  father_table
+where exists((select son_table.id from user_answer_0 son_table  where son_table.id > 1893229443865804800 limit 10) as tmp_table  where father_table.id=tmp_table.id)
+
+# 错误
+select *
+from user_answer_0  father_table
+where exists((select son_table.id from user_answer_0 son_table  where son_table.id > 1893229443865804800 limit 10) as tmp_table  where father_table.id=tmp_table.id)
+
+
+
+SELECT *
+FROM user_answer_0 father_table
+WHERE EXISTS (
+    SELECT 1
+    FROM user_answer_0 son_table
+    WHERE
+        son_table.id > 1893229443865804800
+      AND father_table.id = son_table.id  -- 关联条件写在子查询内部
+
+) LIMIT 10;
+
+SELECT id
+FROM user_answer_0 son_table
+WHERE
+    son_table.id > 1893229443865804800
+limit 10
+
+select *
+from user_answer_0  father_table
+where id > (select id from  user_answer_0 where id >1893229443865804800 limit 1) limit 10
---
+++ sql/group1/dir1/cata1/folder1/springStudy1/goup1/dir1/cata1/springStudy1DeepPage1.sql
@@ -0,0 +1,8 @@
+use yudada_2
+
+select count(*)
+from user_answer_0
+
+explain
+select *
+from user_answer_0  limit 1,20
