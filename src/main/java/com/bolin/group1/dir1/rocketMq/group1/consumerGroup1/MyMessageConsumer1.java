05bafdac5fef02ff89f4e54f6d892af20da24b2e
bolin <1607944055@qq.com>
2025-03-03 20:01:09 +0800

    将消息从 UNSPECIFIED 切换到普通消息

--- src/main/java/com/bolin/group1/dir1/rocketMq/group1/consumerGroup1/MyMessageConsumer1.java
+++ src/main/java/com/bolin/group1/dir1/rocketMq/group1/consumerGroup1/MyMessageConsumer1.java
@@ -4,13 +4,17 @@ import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
 import org.apache.rocketmq.spring.core.RocketMQListener;
 import org.springframework.stereotype.Component;
 
-//@Component
-//@RocketMQMessageListener(topic = "TestTopic",selectorExpression = "messageTag",consumerGroup = "my-consumer_group_1_1")
+@Component
+@RocketMQMessageListener(topic = "normalTopic1",selectorExpression = "messageTag",consumerGroup = "my-consumer_group_1_1")
 public class MyMessageConsumer1 implements RocketMQListener<String> {
-
+    int i=0;
     @Override
     public void onMessage(String message) {
+
         // 处理消息的逻辑
-        System.out.println("接收到消息: " + "my-consumer_group_1_1"+message);
+        i++;
+        System.out.println("第"+i+"次");
+        throw new RuntimeException("消费失败");
+//        System.out.println("接收到消息: " + "my-consumer_group_1_1"+message);
     }
 }
