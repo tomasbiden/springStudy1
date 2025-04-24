d26ccc506672055ab868d819ac309752ee779add
barry.li <barry.li@tec-do.com>
2025-04-24 18:54:45 +0800

    CommandLineRunner 接口可以进行全部bean注入完毕后再进行初始化

---
+++ src/main/java/com/bolin/group2/dir1/cata1/init/CacheInitService.java
@@ -0,0 +1,10 @@
+package com.bolin.group2.dir1.cata1.init;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class CacheInitService implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
---
+++ src/main/java/com/bolin/group2/dir1/cata1/init/CommandLineRunnerExample.java
@@ -0,0 +1,39 @@
+package com.bolin.group2.dir1.cata1.init;
+
+import com.bolin.group2.dir1.cata1.demos.pojo.App;
+import com.bolin.group2.dir1.cata1.service.AppService;
+import com.bolin.group2.dir1.cata1.service.UserAnswerService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * 执行时机​​：
+ * 比 @PostConstruct 更晚（所有bean已初始化完成）
+ * 比 ApplicationReadyEvent 更早（应用尚未完全就绪）
+ */
+
+/**
+ * 
+ */
+
+@SpringBootApplication
+public class CommandLineRunnerExample implements  CommandLineRunner {
+
+    @Autowired
+    private AppService appServiceImpl;
+
+
+    // CommandLineRunner 示例
+
+    public void run(String... args) throws Exception {
+//        这里可以进行初始化，加载数据
+        App app = appServiceImpl.test1();
+    }
+
+}
+
+
+
