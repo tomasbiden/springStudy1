1d41b294de43580925526ebbe9dd5e9a3bc3eda6
barry.li <barry.li@tec-do.com>
2025-05-07 16:25:39 +0800

    easyExcel工具类

---
+++ src/main/java/com/bolin/group2/dir1/cata1/util/excel/converter/ManualDataPercentConverter.java
@@ -0,0 +1,62 @@
+package com.bolin.group2.dir1.cata1.util.excel.converter;
+
+import cn.hutool.core.util.StrUtil;
+import com.alibaba.excel.converters.Converter;
+import com.alibaba.excel.enums.CellDataTypeEnum;
+import com.alibaba.excel.metadata.GlobalConfiguration;
+import com.alibaba.excel.metadata.data.ReadCellData;
+import com.alibaba.excel.metadata.data.WriteCellData;
+import com.alibaba.excel.metadata.property.ExcelContentProperty;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+/**
+ * 对百分比做特殊处理：读取时自动去除百分号；导出时自动加上百分号
+ */
+public class ManualDataPercentConverter implements Converter<BigDecimal> {
+    @Override
+    public Class<BigDecimal> supportJavaTypeKey() {
+        return BigDecimal.class;
+    }
+
+    @Override
+    public CellDataTypeEnum supportExcelTypeKey() {
+        return CellDataTypeEnum.STRING;
+    }
+
+    /**
+     * 这里读的时候会调用
+     * 将百分比转化为纯数字文本
+     *
+     * @param cellData            NotNull
+     * @param contentProperty     Nullable
+     * @param globalConfiguration NotNull
+     * @return
+     */
+    @Override
+    public BigDecimal convertToJavaData(ReadCellData<?> cellData, ExcelContentProperty contentProperty, GlobalConfiguration globalConfiguration) throws Exception {
+        String value = cellData.getStringValue();
+        if (StrUtil.isBlank(value)) {
+            return cellData.getNumberValue();
+        }
+        if (value.endsWith("%")) {
+            value = StrUtil.removeSuffix(value, "%");
+        }
+        return new BigDecimal(value);
+    }
+
+    /**
+     * 这里是写的时候会调用
+     *
+     * @param value               NotNull
+     * @param contentProperty     Nullable
+     * @param globalConfiguration NotNull
+     * @return
+     */
+    @Override
+    public WriteCellData<?> convertToExcelData(BigDecimal value, ExcelContentProperty contentProperty,
+                                               GlobalConfiguration globalConfiguration) throws Exception {
+        return new WriteCellData<>(value.stripTrailingZeros().multiply(new BigDecimal(100)).setScale(2, RoundingMode.DOWN).toPlainString() + "%");
+    }
+}
